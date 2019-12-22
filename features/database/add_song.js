3f2b6f95c39bdb6732e2e1ef86c8e5b74e3d9a56
OuiOuiCroissant <50111424+OuiOuiCroissant@users.noreply.github.com>
2019-12-23 01:46:29 +0200

    unique songIDs in mongodb

--- features/database/add_song.js
+++ features/database/add_song.js
@@ -12,8 +12,8 @@ module.exports = function addSong(userId, myLyrics) {
     if (err) throw err;
     if (res) {
       lyrics = new Set(res.myLyrics);
-      myLyrics = Array.from(myLyrics);
-      Listener.findByIdAndUpdate(userId, { myLyrics },
+      //myLyrics = Array.from(myLyrics);
+      Listener.findByIdAndUpdate(userId, { myLyrics: lyrics },
       { new: true }, (err, res) => {
         if (err) throw err;
       });
