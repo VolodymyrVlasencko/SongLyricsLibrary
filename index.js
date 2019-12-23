acc2a299f4d8ded93ed583f1c287a2772936184d
OuiOuiCroissant <50111424+OuiOuiCroissant@users.noreply.github.com>
2019-12-23 12:24:15 +0200

    fixing rendering issues

--- index.js
+++ index.js
@@ -300,7 +300,7 @@ app.get('/library', isAuthenticated, (req, res) => {
       });
     });
   }
-  getLibItemList();
+  getLibItemList(libItems, songList);
   res.render('index_lib', { libItems: libItems });
 });
 
