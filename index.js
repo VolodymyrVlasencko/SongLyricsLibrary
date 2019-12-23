687999ffb71dabe9d5ecdc092f6c7a7d53df2357
OuiOuiCroissant <50111424+OuiOuiCroissant@users.noreply.github.com>
2019-12-23 11:40:35 +0200

    added mongo methods to library

--- index.js
+++ index.js
@@ -284,10 +284,22 @@ app.get('/song/:id', (req, res) => {
 
 app.get('/library', isAuthenticated, (req, res) => {
   let libItems = [];
-  getSongList(req.session.passport.user);
-  console.log(songList);
 
-  res.render('index_lib'/*, { libItems: libItems }*/)
+  getSongList(req.session.passport.user);
+
+  songList.forEach(item => {
+    genius.song(item).then(function(response) {
+      let libItemToPush = {
+        songName: response.song.title,
+        singer: response.song.primary_artist.name,
+        img: response.song.album.name,
+        id: item
+      }
+      libItems.push(libItemToPush)
+    });
+  });
+  console.log(libItems);
+  res.render('index_lib', { libItems: libItems })
 });
 
 
