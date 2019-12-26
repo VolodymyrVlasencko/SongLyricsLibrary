bba23628ba3f6c07316e1356a9f031ed0009791b
OuiOuiCroissant <50111424+OuiOuiCroissant@users.noreply.github.com>
2019-12-26 22:56:42 +0200

    fixed mongo delete item

--- features/database/delete_song.js
+++ features/database/delete_song.js
@@ -7,7 +7,7 @@ module.exports = function deleteSong(userId, songId) {
   mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true });
 
   Listener.findByIdAndUpdate(userId,
-    { $pull: { myLyrics: { $eq: songId } } },
+    { $pull: { myLyrics: { $in: songId } } },
     { new: true }, (err, res) => {
     if (err) throw err;
   });
