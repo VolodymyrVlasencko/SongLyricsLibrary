3ec5b58859c7d6bfa8145ffdb7f6c431866b027a
OuiOuiCroissant <50111424+OuiOuiCroissant@users.noreply.github.com>
2019-12-29 17:19:08 +0200

    adding check to mongodb

--- index.js
+++ index.js
@@ -114,7 +114,7 @@ passport.use('signup', new LocalStrategy({
 
           newListener.username = username;
           newListener.password = createHash(password);
-          newListener.myLyrics = null
+          newListener.myLyrics.push(null)
 
           newListener.save(function(err) {
             if (err){
